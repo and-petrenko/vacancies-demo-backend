1f3afc6d7594a04c033941a5c4bded1af21c7daa
mw79 <mw79@bk.ru>
2018-09-07 13:59:31 +0300

    Access-Control-Allow-Origin error fixed.

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ var app = express();
 const PORT = 4321;
 
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/employers', function(req, res) {
   res.send(employerService.get());
